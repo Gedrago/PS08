45a6e2cbab63262101fc223ba260737ddad8e757
Gedrago <mgedrago18@amherst.edu>
2017-11-02 00:17:46 -0400

    revision

--- PS08.R
+++ PS08.R
@@ -35,8 +35,8 @@ head(train)
 # knn modeling ------------------------------------------------------------
  
 # Values to use: setting n to number from 0 to 10000 with 500 increments, and setting k to numbers from 0 to 10 with 1 incremenents  
-n_values <- c(seq(0, 1000000, 500))
-k_values <- c(seq(0, 5, 1))
+n_values <- c(seq(500, 100000, 5000))
+k_values <- c(seq(1, 5, 1))
 runtime_dataframe <- expand.grid(n_values, k_values) %>%
   as_tibble() %>%
   rename(n=Var1, k=Var2) %>%
@@ -45,36 +45,39 @@ runtime_dataframe
 
 
 # Time knn here -----------------------------------------------------------
+model_formula <- as.formula(Device ~ X + Y + Z  )
 
+
+totalTimes = c()
 for (i in 1:length(k_values)) {
   #go through the each of the k_values 
   for ( j in 1:length(n_values)) {
     #go through the each of the n_values
     #slice the train set at the jth value of n_value we are on 
     trainingset <- slice(train, 1:n_values[j])
-    a <- 0
-    for (d in 1:(length(n_values)*length(k_values))) {
-      tic()
-      model_formula <- as.formula(Device ~ X + Y + Z  )
-      model_knn <- caret::knn3(model_formula, data=trainingset , k =  k_values[i])
+    tic()
+    model_knn <- caret::knn3(model_formula, data=trainingset , k =  k_values[i])
       timer <- toc()
       x <- timer$toc - timer$tic
-      a[d] <- x
-    }
+      totalTimes = c(totalTimes, x)
   }
 }
 
-runtime_dataframe[,3] <- a 
-runtime_dataframe
+ 
+runtime_dataframe <- expand.grid(n_values, k_values) %>%
+  as_tibble() %>%
+  rename(n=Var1, k=Var2) %>%
+  mutate(runtime = totalTimes)
+runtime_dataframe 
+
 
 # Plot your results ---------------------------------------------------------
 # Think of creative ways to improve this barebones plot. Note: you don't have to
 # necessarily use geom_point
-runtime_plot <- ggplot(runtime_dataframe, aes(x=n, y=k, col=runtime))  +  geom_point()
+runtime_plot <- ggplot(runtime_dataframe, aes(x=n, y=totalTimes, col=k)) +  geom_point( )
 
 runtime_plot
 ggsave(filename="meron_gedrago.png", width=16, height = 9)
-
 
 
 
@@ -85,7 +88,7 @@ ggsave(filename="meron_gedrago.png", width=16, height = 9)
 # -k: number of neighbors to consider
 # -p: number of predictors used? In this case p is fixed at 3
 
-#Big-O runtime complexity : O(n/K)
+#Big-O runtime complexity : O(n)  because the graph displays positive linear relation between the runtime and the n value.  
 
 
 
